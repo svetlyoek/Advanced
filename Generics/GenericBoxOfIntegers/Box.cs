c7bde55d88bf17e15249c5bade3ec0c373994df1
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-12 19:42:20 +0300

    Add new

---
+++ Generics/GenericBoxOfIntegers/Box.cs
@@ -0,0 +1,25 @@
+﻿
+namespace GenericBoxOfIntegers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value
+        {
+            get { return this.value; }
+
+            set { this.value = value; }
+        }
+
+    }
+}
---
+++ Generics/GenericBoxOfIntegers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxOfIntegers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                var box = new Box<int>(number);
+
+                Console.WriteLine(number.GetType() + ":" + " " + box.Value);
+            }
+        }
+    }
+}
