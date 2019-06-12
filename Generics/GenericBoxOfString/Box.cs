6d7372505680642a9c330d985439be9c6db15d51
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-12 16:25:26 +0300

    Add new

---
+++ Generics/GenericBoxOfString/Box.cs
@@ -0,0 +1,34 @@
+﻿
+namespace GenericBoxOfString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value
+        {
+            get { return this.value; }
+
+            set { this.value = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.Append($"{this.Value.GetType()}: { this.Value}");
+
+            return result.ToString();
+        }
+
+    }
+}
---
+++ Generics/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                string text = Console.ReadLine();
+
+                var box = new Box<string>(text).ToString();
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
