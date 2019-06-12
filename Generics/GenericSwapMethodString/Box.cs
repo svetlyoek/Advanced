647c33f4a9a6f386da26c585788a4e9bb3fdc7b3
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-12 22:09:45 +0300

    Add new

---
+++ Generics/GenericSwapMethodString/Box.cs
@@ -0,0 +1,58 @@
+﻿
+namespace GenericSwapMethodString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        private T item;
+        public Box()
+        {
+            this.data = new List<T>();
+            this.Item = item;
+        }
+
+        public List<T> Data
+        {
+            get { return this.data; }
+
+            set { this.data = value; }
+        }
+
+        public T Item
+        {
+            get { return this.item; }
+
+            set { this.item = value; }
+        }
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public void Swap<T>(int firstIndex, int secondIndex)
+        {
+            var temp = this.data[firstIndex];
+            this.data[firstIndex] = this.data[secondIndex];
+            this.data[secondIndex] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ Generics/GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var box = new Box<string>();
+
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                string text = Console.ReadLine();
+
+                box.Add(text);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            box.Swap<string>(firstIndex, secondIndex);
+            Console.WriteLine(box.ToString());
+        }
+
+
+    }
+}
