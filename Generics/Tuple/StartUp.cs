b8a6b16ed0a06186d22794ffdff2d5c9a4b123f8
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-13 17:17:40 +0300

    Add new

---
+++ Generics/Tuple/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] personAdress = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string personName = personAdress[0] + " " + personAdress[1];
+            string adress = personAdress[2];
+
+            string[] nameBeer = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string name = nameBeer[0];
+            int liters = int.Parse(nameBeer[1]);
+
+            string[] integerDouble = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            int integer = int.Parse(integerDouble[0]);
+            double myDouble = double.Parse(integerDouble[1]);
+
+            var personNameTuple = new Tuple<string, string>(personName, adress);
+            var nameBeerTuple = new Tuple<string, int>(name, liters);
+            var integerDoublesTuple = new Tuple<int, double>(integer, myDouble);
+
+            Console.WriteLine(personNameTuple.PrintItems(personName, adress));
+            Console.WriteLine(nameBeerTuple.PrintItems(name, liters));
+            Console.WriteLine(integerDoublesTuple.PrintItems(integer, myDouble));
+        }
+    }
+}
---
+++ Generics/Tuple/Tuple.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Tuple
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Tuple<TItem1, TItem2>
+    {
+        private TItem1 item1;
+
+        private TItem2 item2;
+        public Tuple(TItem1 item1, TItem2 item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public TItem1 Item1
+        {
+            get { return this.item1; }
+
+            set { this.item1 = value; }
+        }
+
+        public TItem2 Item2
+        {
+            get { return this.item2; }
+
+            set { this.item2 = value; }
+        }
+
+        public string PrintItems(TItem1 item1, TItem2 item2)
+        {
+            return $"{item1} -> {item2}";
+        }
+
+    }
+
+}
