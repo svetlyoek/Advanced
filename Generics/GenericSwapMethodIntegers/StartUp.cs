b34d286d634e50c4a87dab97ad56430f5c116892
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-13 10:25:14 +0300

    Add new

---
+++ Generics/GenericSwapMethodIntegers/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodIntegers
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var box = new Box<int>();
+
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                box.Add(number);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            box.Swap<int>(firstIndex, secondIndex);
+            Console.WriteLine(box.ToString());
+        }
+    }
+    }
