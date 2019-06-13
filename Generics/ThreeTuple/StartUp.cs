902644f1227dc701e5f15d562745cef6c77c4a2d
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-13 18:35:36 +0300

    Add new

---
+++ Generics/ThreeTuple/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace ThreeTuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personAdressTown = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string personName = personAdressTown[0] + " " + personAdressTown[1];
+            string adress = personAdressTown[2];
+            string town = personAdressTown[3];
+
+            if (personAdressTown.Length > 4)
+            {
+                town = personAdressTown[3] + " " + personAdressTown[4];
+            }
+
+            string[] nameBeerDrunk = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string name = nameBeerDrunk[0];
+            int liters = int.Parse(nameBeerDrunk[1]);
+            bool isDrunk = false;
+            if (nameBeerDrunk[2] == "drunk")
+            {
+                isDrunk = true;
+            }
+
+            string[] nameBankAccount = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string names = nameBankAccount[0];
+            double balance = double.Parse(nameBankAccount[1]);
+            string bankName = nameBankAccount[2];
+
+            var personAdressTownTuple = new ThreeTuple<string, string, string>(personName, adress, town);
+            var nameBeerDrunkTuple = new ThreeTuple<string, int, bool>(name, liters, isDrunk);
+            var nameBankAccountTuple = new ThreeTuple<string, double, string>(names, balance, bankName);
+
+            Console.WriteLine(personAdressTownTuple.PrintItems(personName, adress, town));
+            Console.WriteLine(nameBeerDrunkTuple.PrintItems(name, liters, isDrunk));
+            Console.WriteLine(nameBankAccountTuple.PrintItems(names, balance, bankName));
+        }
+
+    }
+}
+
---
+++ Generics/ThreeTuple/ThreeTuple.cs
@@ -0,0 +1,48 @@
+﻿
+namespace ThreeTuple
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ThreeTuple<TfirstElement, TsecondElement, TthirdElement>
+    {
+        private TfirstElement firstElement;
+
+        private TsecondElement secondElement;
+
+        private TthirdElement thirdElement;
+        public ThreeTuple(TfirstElement firstElement, TsecondElement secondElement, TthirdElement thirdElement)
+        {
+            this.FirstElement = firstElement;
+            this.SecondElement = secondElement;
+            this.ThirdElement = thirdElement;
+        }
+
+        public TfirstElement FirstElement
+        {
+            get { return this.firstElement; }
+
+            set { this.firstElement = value; }
+        }
+
+        public TsecondElement SecondElement
+        {
+            get { return this.secondElement; }
+
+            set { this.secondElement = value; }
+        }
+
+        public TthirdElement ThirdElement
+        {
+            get { return this.thirdElement; }
+
+            set { this.thirdElement = value; }
+        }
+
+        public string PrintItems(TfirstElement firstElement, TsecondElement secondElement, TthirdElement thirdElement)
+        {
+            return $"{firstElement} -> {secondElement} -> {thirdElement}";
+        }
+    }
+}
