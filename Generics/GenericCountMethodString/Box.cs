87e38e848cd3eec1fcf2d43a48d2d161cc540af2
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-13 11:05:44 +0300

    Add new

---
+++ Generics/GenericCountMethodString/Box.cs
@@ -0,0 +1,50 @@
+﻿
+namespace GenericCountMethodString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        private T item;
+        public Box()
+        {
+            this.data = new List<T>();
+            this.Item = item;
+        }
+
+        public List<T> Data
+        {
+            get { return this.data; }
+
+            set { this.data = value; }
+        }
+
+        public T Item
+        {
+            get { return this.item; }
+
+            set { this.item = value; }
+        }
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Generics/GenericCountMethodString/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var box = new Box<string>();
+
+            int elementCounter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < elementCounter; i++)
+            {
+                string text = Console.ReadLine();
+
+                box.Add(text);
+            }
+
+            string compareItem = Console.ReadLine();
+
+            int count = CompareElements(box.Data, compareItem);
+
+            Console.WriteLine(count);
+        }
+
+        public static int CompareElements<T>(List<T> data, T item)
+         where T : IComparable
+        {
+            int counter = 0;
+
+            foreach (var element in data)
+            {
+                if (element.CompareTo(item) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+
+        }
+    }
+}
