565a1cd2e35c0622c372b2673becea8892e847d2
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-13 13:24:13 +0300

    Add new

---
+++ Generics/GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var box = new Box<double>();
+
+            int elementCounter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < elementCounter; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+
+                box.Add(number);
+            }
+
+            double compareItem = double.Parse(Console.ReadLine());
+
+            int count = CompareElements(box.Data, compareItem);
+
+            Console.WriteLine(count);
+        }
+
+        public static int CompareElements<T>(List<T> data, T item)
+         where T : IComparable
+        {
+            int counter = 0;
+
+            foreach (var element in data)
+            {
+                if (element.CompareTo(item) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+
+        }
+    }
+
+}
