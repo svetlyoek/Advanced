1d9e4dbc51d245a435992f5cc36b706057568ab6
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-14 17:40:31 +0300

    Add new

---
+++ Iterators and comparators/LibraryIterators/Library.cs
@@ -0,0 +1,53 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+
+            private int currentIndex;
+            public Book Current => this.books[this.currentIndex];
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+         => ++this.currentIndex < this.books.Count;
+
+            public void Reset()
+          => this.currentIndex = -1;
+        }
+    }
+}
+
