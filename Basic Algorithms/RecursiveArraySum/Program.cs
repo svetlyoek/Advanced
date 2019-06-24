6bcdd19cd4be527111d22e2f220c17741703f91e
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-24 16:13:25 +0300

    Add new

---
+++ Basic Algorithms/RecursiveArraySum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+            int index = 0;
+
+            Console.WriteLine(Sum(numbers, index));
+        }
+
+        static int Sum(int[] numbers, int index)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + Sum(numbers, index + 1);
+
+        }
+    }
+}
---
+++ Basic Algorithms/RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        static long Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
