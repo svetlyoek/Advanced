cc39ec79fc3442661d4390f877e557005ebfa4cf
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-28 09:30:36 +0300

    Add new

---
+++ Basic Algorithms/BinarySearch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int key = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(IndexOf(numbers, key));
+        }
+
+        private static int IndexOf(int[] numbers, int key)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+
+            while (start <= end)
+            {
+                int middle = end + start / 2;
+
+                if (key < numbers[middle])
+                {
+                    end = middle - 1;
+                }
+                else if (key > numbers[middle])
+                {
+                    start = middle + 1;
+                }
+                else
+                {
+                    return middle;
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
